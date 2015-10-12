bd495bd8abf255054287d8ae0e321e45a53c3519
wangyumi <wangyumi@gmail.com>
2015-10-12 10:53:56 +0800

    update

--- check.sh
+++ check.sh
@@ -1,6 +1,9 @@
-repos=`curl 10.97.232.19/v1/search?q= 2>/dev/null | jq -r '.results | .[] | .name'| sed -e "s/^library\///g" `
+V1_REPO=
+v2_REPO=
+
+repos=`curl $V1_REPO/v1/search?q= 2>/dev/null | jq -r '.results | .[] | .name'| sed -e "s/^library\///g" `
 
 for repo in $repos
 do 
-	curl 10.101.74.64:5000/v2/$repo/tags/list 2>/dev/null |  grep '"tags":null' | cut -d ',' -f 1|cut -d ':' -f 2 |sed -e 's/"//g' 
+	curl $V2_REPO/v2/$repo/tags/list 2>/dev/null |  grep '"tags":null' | cut -d ',' -f 1|cut -d ':' -f 2 |sed -e 's/"//g' 
 done
