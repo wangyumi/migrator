c15b67fd37849e4d3611a542d7d80bbd72e66a26
wangyumi <wangyumi@gmail.com>
2015-10-12 10:51:15 +0800

    Update run.sh
    update

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@ for i in a b c d e f g h i j k m n o p q r s t u v w x y z
 do 
 docker run -it \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  -e V1_REGISTRY=reg.docker.alibaba-inc.com \
-  -e V2_REGISTRY=10.101.74.64:5000 \
+  -e V1_REGISTRY= \
+  -e V2_REGISTRY= \
   -e NO_LOGIN=true \
   -e ERROR_ACTION=skip \
   -e V1_REPO_FILTER=^$i \
